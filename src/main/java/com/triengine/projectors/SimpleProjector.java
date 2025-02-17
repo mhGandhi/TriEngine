33024e1ac3d6b3576a8629dc2760fedb7cb154bb
Adrian <adrian.akipi@web.de>
2025-02-17 03:03:32 +0100

    fixed z; now properly along camera axis

--- src/main/java/com/triengine/projectors/SimpleProjector.java
+++ src/main/java/com/triengine/projectors/SimpleProjector.java
@@ -1,5 +1,6 @@
 package com.triengine.projectors;
 
+import com.triengine.Axis;
 import com.triengine.Vec;
 import com.triengine.projectors.viewstates.ViewState;
 
@@ -15,12 +16,13 @@ public class SimpleProjector extends Projector{
 
     @Override
     public int[] project(Vec pSysPos) {
-        pSysPos = rotateAroundX(pSysPos, Math.toRadians(svs.angleX));
-        pSysPos = rotateAroundY(pSysPos, Math.toRadians(svs.angleY));
-        pSysPos = rotateAroundZ(pSysPos, Math.toRadians(svs.angleZ));
+        pSysPos = rotate(pSysPos, svs.angleX, Axis.X);
+        pSysPos = rotate(pSysPos, svs.angleY, Axis.Y);
+        pSysPos = rotate(pSysPos, svs.angleZ, Axis.Z);
 
         int rX = 0;
         int rY = 0;
+        int rZ = 0;
 
         rX += (int)(pSysPos.y);
         rY -= (int)(pSysPos.z);
@@ -28,31 +30,35 @@ public class SimpleProjector extends Projector{
         rX -= (int)(pSysPos.x * (0.707/2));
         rY += (int)(pSysPos.x * (0.707/2));
 
-        return new int[] {rX,rY};
+
+        Vec frontDir = Vec.o().x(1).y((0.707/2)).z((0.707/2)).normalize();
+        rZ = (int)pSysPos.dot(frontDir);
+
+        return new int[] {rX,rY,rZ};
     }
 
-    private static Vec rotateAroundX(Vec point, double angleRadians) {
-        double y = point.y * Math.cos(angleRadians) - point.z * Math.sin(angleRadians);
-        double z = point.y * Math.sin(angleRadians) + point.z * Math.cos(angleRadians);
+    private static Vec rotate(Vec point, double pAngle, Axis pAxis){
+        double angleRadians = Math.toRadians(pAngle);
+        switch (pAxis){
+            case X -> {
+                double y = point.y * Math.cos(angleRadians) - point.z * Math.sin(angleRadians);
+                double z = point.y * Math.sin(angleRadians) + point.z * Math.cos(angleRadians);
+                return new Vec(point.x, y, z);
+            }
+            case Y -> {
+                double x = point.x * Math.cos(angleRadians) + point.z * Math.sin(angleRadians);
+                double z = -point.x * Math.sin(angleRadians) + point.z * Math.cos(angleRadians);
+                return new Vec(x, point.y, z);
+            }
+            case Z -> {
+                double x = point.x * Math.cos(angleRadians) - point.y * Math.sin(angleRadians);
+                double y = point.x * Math.sin(angleRadians) + point.y * Math.cos(angleRadians);
+                return new Vec(x, y, point.z);
+            }
+        }
 
-        // Return the new point
-        return new Vec(point.x, y, z);
-    }
-    private static Vec rotateAroundY(Vec point, double angleRadians) {
-        // Apply rotation matrix for Y-axis
-        double x = point.x * Math.cos(angleRadians) + point.z * Math.sin(angleRadians);
-        double z = -point.x * Math.sin(angleRadians) + point.z * Math.cos(angleRadians);
-
-        // Return the new point
-        return new Vec(x, point.y, z);
-    }
-    private static Vec rotateAroundZ(Vec point, double angleRadians) {
-        // Apply rotation matrix for Z-axis
-        double x = point.x * Math.cos(angleRadians) - point.y * Math.sin(angleRadians);
-        double y = point.x * Math.sin(angleRadians) + point.y * Math.cos(angleRadians);
-
-        // Return the new point
-        return new Vec(x, y, point.z);
+        System.err.println("Invalid Axis, reverting to nullvector");
+        return Vec.o();
     }
 
     public static class SimpleViewState extends ViewState {
